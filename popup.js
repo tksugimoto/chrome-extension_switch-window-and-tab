a09ae95a3fc7f490ffb94e41130800b824de6ad6
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-01-08 22:00:15 +0900

    [Add] 全角入力を半角入力に変換する設定追加

--- popup.js
+++ popup.js
@@ -14,6 +14,12 @@ function setupCheckBox(id) {
 
 var displayScreenshot = setupCheckBox("display-screenshot");
 var popupWindowFirst = setupCheckBox("popup-window-first");
+var allow_only_half_width_char = (elem => {
+	elem.addEventListener("change", evt => {
+		allow_only_half_width_char = evt.checked;
+	});
+	return elem.checked;
+})(setupCheckBox("allow-only-half-width-char"));
 
 chrome.windows.getAll({
 	// tab情報を含める
@@ -230,11 +236,37 @@ searchWordInput.addEventListener("keyup", evt => {
 		container_bookmarks.style.display = "none";
 	}
 });
+
+// 全角入力を半角に変換する
+// TODO: 完全対応
+const codeToKeyMap = {
+	"Minus": "-",
+	"IntlRo": "_",
+	"Period": ".",
+	"Comma": ",",
+	"BracketLeft": "@" 
+};
+for (let i = 0; i < 10; i++) {
+	codeToKeyMap[`Numpad${i}`] = codeToKeyMap[`Digit${i}`] = i.toString();
+}
+"abcdefghijklmnopqrstuvwxyz".split("").forEach(char => {
+	codeToKeyMap[`Key${char.toUpperCase()}`] = char;
+});
+
 searchWordInput.addEventListener("keydown", evt => {
 	// Escが押されたら入力欄を空にする
 	//   ※keyupだと日本語入力で未確定状態からEscでも反応してしまう
 	if (evt.keyCode === 27) {
 		searchWordInput.value = "";
+	}
+
+	if (allow_only_half_width_char && evt.key === "Process") {
+		const code = evt.code;
+		const key = codeToKeyMap[code];
+		if (!key) return;
+
+		document.execCommand("insertText", null, key);
+		evt.preventDefault();
 	}
 });
 
