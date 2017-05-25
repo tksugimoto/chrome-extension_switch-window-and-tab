bf0ffdbe19279bc2bec9aa67e88430d6f1cd90f4
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-05-25 23:27:23 +0900

    [Update] tab-link web-componentsをclass構文での定義に変更

--- chrome-extension/web-components/tab-link.js
+++ chrome-extension/web-components/tab-link.js
@@ -6,43 +6,47 @@
 	
 	var template = thisDoc.querySelector('template').content;
 	
-	var MyElementProto = window.Object.create(window.HTMLElement.prototype);
+	class TabLinkElement extends HTMLElement {
 
-	MyElementProto.createdCallback = function () {
-		this.createShadowRoot();
-		
-		var clone = thatDoc.importNode(template, true);
-		this.shadowRoot.appendChild(clone);
-	};
+		constructor() {
+			super();
+		}
 
-	MyElementProto.setTab = function (tab) {
-		// TODO: 2回以上setされた時の対策
-		this.shadowRoot.getElementById("container").href = tab.url;
-		this.shadowRoot.getElementById("title").innerText = tab.title || tab.url;
-		if (typeof tab.favIconUrl === "string") {
-			var icon = this.shadowRoot.getElementById("icon");
-			icon.src = tab.favIconUrl;
-			icon.addEventListener("error", () => {
-				icon.style.display = "none";
+		createdCallback() {
+			this.createShadowRoot();
+			
+			var clone = thatDoc.importNode(template, true);
+			this.shadowRoot.appendChild(clone);
+		}
+
+		setTab(tab) {
+			// TODO: 2回以上setされた時の対策
+			this.shadowRoot.getElementById("container").href = tab.url;
+			this.shadowRoot.getElementById("title").innerText = tab.title || tab.url;
+			if (typeof tab.favIconUrl === "string") {
+				var icon = this.shadowRoot.getElementById("icon");
+				icon.src = tab.favIconUrl;
+				icon.addEventListener("error", () => {
+					icon.style.display = "none";
+				});
+			}
+			
+			var link = this.shadowRoot.getElementById("container");
+			link.addEventListener("click", (evt) => {
+				evt.preventDefault();
+				chrome.tabs.update(tab.id, {
+					active: true
+				});
+				chrome.windows.update(tab.windowId, {
+					focused: true
+				});
+			});
+			this.addEventListener("click", () => {
+				link.click();
 			});
 		}
-		
-		var link = this.shadowRoot.getElementById("container");
-		link.addEventListener("click", (evt) => {
-			evt.preventDefault();
-			chrome.tabs.update(tab.id, {
-				active: true
-			});
-			chrome.windows.update(tab.windowId, {
-				focused: true
-			});
-		});
-		this.addEventListener("click", () => {
-			link.click();
-		});
-	};
+
+	}
 	
-	thatDoc.registerElement("tab-link", {
-		prototype: MyElementProto
-	});
+	thatDoc.registerElement("tab-link", TabLinkElement);
 })(window, document);
