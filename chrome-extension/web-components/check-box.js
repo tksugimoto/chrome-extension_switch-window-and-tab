ba93d815c5f76e54351ccd71713aab2cba1242b1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-10-20 23:08:21 +0900

    [Update] web-components <check-box> を更新
    https://github.com/tksugimoto/my-web-components/tree/6898620e8a13c1d964815ad195b4428d4e47c6fc/check-box

--- chrome-extension/web-components/check-box.js
+++ chrome-extension/web-components/check-box.js
@@ -18,35 +18,64 @@
 			
 			this._checkbox = shadowRoot.querySelector('input');
 			
-			if (this.hasAttribute('checked')) {
-				this._checkbox.checked = true;
-			}
-			if (this.hasAttribute('disabled')) {
-				this._checkbox.disabled = true;
-			}
-			
 			this._checkbox.addEventListener('change', () => {
-				this.updateCheckedAttribute();
+				const checked = this._checkbox.checked;
+				this._updateCheckedAttribute(checked);
 				const event = new window.Event('change');
-				event.checked = this._checkbox.checked;
+				event.checked = checked;
 				this.dispatchEvent(event);
 			});
 		}
 
+		static get observedAttributes() {
+			return [
+				'checked',
+				'disabled',
+				'accesskey',
+			];
+		}
+
+		attributeChangedCallback(name, oldValue, newValue) {
+			if (name === 'checked') {
+				const checked = newValue !== null;
+				this._checkbox.checked = checked;
+			} else if (name === 'disabled') {
+				const disabled = newValue !== null;
+				this._checkbox.disabled = disabled;
+			} else if (name === 'accesskey') {
+				if (newValue) {
+					this._checkbox.setAttribute('accesskey', newValue);
+				} else {
+					this._checkbox.removeAttribute('accesskey');
+				}
+			}
+		}
+
 		set checked(value) {
-			this._checkbox.checked = !!value;
-			this.updateCheckedAttribute();
+			const checked = !!value;
+			this._updateCheckedAttribute(checked);
 		}
 		get checked() {
 			return this._checkbox.checked;
 		}
 
-		updateCheckedAttribute() {
-			if (this._checkbox.checked) {
-				this.setAttribute('checked', true);
+		_updateCheckedAttribute(checked) {
+			if (checked) {
+				this.setAttribute('checked', '');
 			} else {
 				this.removeAttribute('checked');
 			}
+		}
+
+		set disabled(value) {
+			if (value) {
+				this.setAttribute('disabled', '');
+			} else {
+				this.removeAttribute('disabled');
+			}
+		}
+		get disabled() {
+			return this._checkbox.disabled;
 		}
 	}
 
